a383787ecd0eae2c47a2fae03b330ca11bb0c97f
LTurnhoff <lisa.turnhoff@rwth-aachen.de>
2018-01-29 09:48:43 +0100

    Created Function GetCellResonseData. Modified DuplicationHandler and CellResponseProcessor. User-defined function part not working yet

---
+++ R/GetCellResponseData.R
@@ -0,0 +1,24 @@
+#' Get Cell Line Drug Response from FORESEE Object
+#'
+#' The GetCellResponseData Function extracts the features of the drug response data that are relevant to predict the drug response.
+#'
+#' @param TrainObject Object that contains all data needed to train a model, such as gene expression, mutation, copy number variation, methylation, cancer type, drug response data, etc.
+#' @param DrugName Name of the drug whose efficacy is supposed to be predicted with the model
+#' @param CellResponseType Format of the drug response data of the TrainObject, such as IC50, AUC, GI50, etc., that is used for prediction
+#'
+#' @return \item{TrainObject}{The TrainObject with extracted drug response data.}
+#' @export
+
+GetCellResponseData <- function(TrainObject, DrugName, CellResponseType){
+
+  DrugResponse <- TrainObject[[CellResponseType]][,grep(DrugName,colnames(TrainObject[[CellResponseType]]))]
+  DrugResponse <- DrugResponse[is.na(DrugResponse)==FALSE]
+  TrainObject[["DrugResponse"]] <- DrugResponse
+
+  # Return gene expression of only those cell lines that have drug response values
+  CommonCelllines <- colnames(TrainObject$GeneExpression)[colnames(TrainObject$GeneExpression) %in% names(TrainObject$DrugResponse)]
+  TrainObject$GeneExpression <- TrainObject$GeneExpression[,CommonCelllines]
+  TrainObject$DrugResponse <- TrainObject$DrugResponse[CommonCelllines]
+
+  return(TrainObject)
+}

fa7348b4f7d19ca5bde96e261164e19b8b252f99
Esfahani <ali.hadizadeh@rwth-aachen.de>
2018-01-31 11:32:27 +0100

    Mid-preparing GDSC

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Genomics of Drug Sensitivity in Cancer or GDSC
+#'
+#' A dataset containing drug responses on cancer celllines.
+#'
+#'
+#' @format ForeseeCell object
+#' \describe{
+#'   \item{GeneExpression}{Gene Expression Matrix}
+#'   \item{AUC}{...}
+#'   ...
+#' }
+#' @source \url{http://www.cancerrxgene.org}
+"GDSC"

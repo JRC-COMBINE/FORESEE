6bff9aa4386cfe0f8a689471f3a18dc24e904117
Esfahani <ali.hadizadeh@rwth-aachen.de>
2018-02-05 14:45:32 +0100

    Early version of DataDownloader added that downloads the last version of GDSC and GSE6434, the commited Data removed

--- data-raw/DataDownloader.R
+++ data-raw/DataDownloader.R
@@ -1,4 +1,4 @@
 ##Downloading the files inside data/ automatically:
 
 download.file(url = "https://osf.io/fwm37/download", destfile = "data/GDSC.rda")
-download.file(url = "https://osf.io/nhw4z/download", destfile = "data/GDSC.rda")
+download.file(url = "https://osf.io/nhw4z/download", destfile = "data/GSE6434.rda")

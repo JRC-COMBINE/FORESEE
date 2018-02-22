c24a4d0c3589aaab29014766101eeef4188bb7cc
Esfahani <ali.hadizadeh@rwth-aachen.de>
2018-02-22 10:42:28 +0100

    Corrected CCLE, Added DAEMEN

--- data-raw/DataDownloader.R
+++ data-raw/DataDownloader.R
@@ -1,5 +1,6 @@
 ##Downloading the files inside data/ automatically:
 
 download.file(url = "https://osf.io/g8fsr/download", destfile = "data/GDSC.rda")
-download.file(url = "https://osf.io/r7xsw/download", destfile = "data/CCLE.rda")
+download.file(url = "https://osf.io/s43jz/download", destfile = "data/CCLE.rda")
 download.file(url = "https://osf.io/nhw4z/download", destfile = "data/GSE6434.rda")
+download.file(url = "https://osf.io/25gm7/download", destfile = "data/DAEMEN.rda")

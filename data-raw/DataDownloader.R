a62eed696875b46d4768ad563ece3386be254dab
Esfahani <ali.hadizadeh@rwth-aachen.de>
2018-02-28 09:24:26 +0100

    Added some patient data

--- data-raw/DataDownloader.R
+++ data-raw/DataDownloader.R
@@ -2,5 +2,13 @@
 
 download.file(url = "https://osf.io/g8fsr/download", destfile = "data/GDSC.rda")
 download.file(url = "https://osf.io/s43jz/download", destfile = "data/CCLE.rda")
-download.file(url = "https://osf.io/nhw4z/download", destfile = "data/GSE6434.rda")
 download.file(url = "https://osf.io/25gm7/download", destfile = "data/DAEMEN.rda")
+
+download.file(url = "https://osf.io/nhw4z/download", destfile = "data/GSE6434.rda")
+download.file(url = "https://osf.io/nh6uy/download", destfile = "data/EGEOD18864.rda")
+download.file(url = "https://osf.io/uxr6k/download", destfile = "data/GSE33072_erlotinib.rda")
+download.file(url = "https://osf.io/tfw7r/download", destfile = "data/GSE33072_sorafenib.rda")
+download.file(url = "https://osf.io/xc7n9/download", destfile = "data/GSE9782_GPL96_bortezomib.rda")
+download.file(url = "https://osf.io/6ubcq/download", destfile = "data/GSE9782_GPL96_dexamethasone.rda")
+download.file(url = "https://osf.io/wchvn/download", destfile = "data/GSE9782_GPL97_bortezomib.rda")
+download.file(url = "https://osf.io/jqdgy/download", destfile = "data/GSE9782_GPL97_dexamethasone.rda")

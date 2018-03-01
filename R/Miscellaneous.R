c6625450730499923f5f78514eb507ff68789b66
Esfahani <ali.hadizadeh@rwth-aachen.de>
2018-03-01 12:47:47 +0100

    Added 'listCellLines' and 'listDrugs'

--- R/Miscellaneous.R
+++ R/Miscellaneous.R
@@ -23,3 +23,34 @@ listInputOptions <- function(METHOD){
   }
   return(listOfPossibilitiesWithOutMETHODName)
 }
+
+#' List All Cell lines Inside a ForeseeTrain Object
+#'
+#' listCellLines returns all cell lines available in a ForeseeTrain Object.
+#'
+#' @param OBJ A ForeseeTrain object that you want to extract its containing cell lines.
+#'
+#' @return \item{Character vector of all cell line names in OBJ.}
+#' @examples
+#' listCellLines(GDSC)
+#' @export
+
+listCellLines <- function(OBJ){
+  return(colnames(OBJ$GeneExpression))
+}
+
+#' List All Drugs Inside a ForeseeTrain Object
+#'
+#' listDrugs returns all possible drugs that were tested and has reponses available in a ForeseeTrain Object.
+#'
+#' @param OBJ A ForeseeTrain object that you want to extract all possible drugs it has response information on.
+#'
+#' @return \item{Character vector of all drug names in OBJ.}
+#' @examples
+#' listDrugs(GDSC)
+#' @export
+
+listDrugs <- function(OBJ){
+  if(!any(names(OBJ) == "IC50")) stop("listDrugs is not implemented completely!!")
+  return(colnames(OBJ$IC50)) ## This will break when OBJ doesn't have IC50 -> ToDO: have to have a better 'Universal' slot available in all Train Objects
+}
